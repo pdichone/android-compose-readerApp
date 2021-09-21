26b801661e202e8694c230d6f5fd5aa272f0d519
pdichone <pdichone@gmail.com>
2021-09-21 12:13:03 -0700

    first commit

---
+++ app/src/main/java/com/bawp/areader_test/screens/WelcomeScreen.kt
@@ -0,0 +1,23 @@
+package com.bawp.areader_test.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun WelcomeScreen() {
+    androidx.compose.material.Surface(
+        modifier = Modifier.padding(10.dp),
+                                    color = Color.White,
+                                     shape = CircleShape) {
+
+
+    }
+
+}
